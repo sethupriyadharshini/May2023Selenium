1600234003e933f87f8a37563d3b404c71aacc22
sethupriyadharshini <sethupriyadharshinis@gmail.com>
2023-05-07 12:12:24 +0530

    First Commit for StaleElementException

--- src/main/java/org/example/StaleElementReferenceExample.java
+++ src/main/java/org/example/StaleElementReferenceExample.java
@@ -4,21 +4,43 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class StaleElementReferenceExample {
 
+    public static WebDriver driver;
+    public static void waitMethod(By locator)
+    {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+        wait.until(ExpectedConditions.refreshed(ExpectedConditions.presenceOfElementLocated(locator)));
+        driver.findElement(locator).sendKeys("completed");
+    }
+
+    public static void waitMethod2(By locator)
+    {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+        wait.until(ExpectedConditions.refreshed(ExpectedConditions.presenceOfElementLocated(locator)));
+        driver.findElement(locator).click();
+    }
     public static void main(String[] args){
 
-        WebDriver driver = new ChromeDriver();
+        driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://www.lambdatest.com/selenium-playground/");
-        WebElement search_click = driver.findElement(By.linkText("Table Data Search"));
-        search_click.click();
-        WebElement filter = driver.findElement(By.id("task-table-filter"));
-        filter.sendKeys("in progress");
+        By search_click = By.linkText("Table Data Search");
+        WebElement search1_click = driver.findElement(search_click);
+        search1_click.click();
+        By filter = By.id("task-table-filter");
+        WebElement filter1 = driver.findElement(filter);
+        filter1.sendKeys("in progress");
         driver.navigate().back();
-        search_click.click();
-        filter.sendKeys("in progress");
+        waitMethod2(search_click);
+        waitMethod(filter);
+        //search_click.click();
+        //filter.sendKeys("in progress");
     }
 
 }
